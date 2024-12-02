d4d3064b6249b230c0f4292971ee92769d81b555
Vagapng <opeyemisemiat2019@gmail.com>
2024-12-02 08:03:42 +0100

    Update windbglib.py

--- windbglib.py
+++ windbglib.py
@@ -1,6 +1,6 @@
 """
 Copyright (c) 2011-2022, Peter Van Eeckhoutte - Corelan Consulting bv
-All rights reserved.
+All rights reserved 
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
